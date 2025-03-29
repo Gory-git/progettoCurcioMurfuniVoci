65e88d35e2a6e803ed3db7da9bd762dc5aea29ab
SalvatoreCurcio23 <salvatorecurcio03@gmail.com>
2025-03-29 13:11:08 +0100

    Signature dei metodi

---
+++ trustrank32c.c
@@ -0,0 +1,32 @@
+float* selectSeed(float** tranMatInv, int numP, float alfaI, int mI){
+
+}
+
+int* rank(int* index, float* s){
+
+}
+
+int oracle(int index){
+
+}
+
+float* normalize(float* scoreDistVect){
+
+}
+
+float* computeScores(float** tranMat, float alfaB, int mB, float* scoreDistVect){
+
+}
+
+float** reverseMat(float** mat){
+    
+}
+
+float** trustrank(float** tranMat, int numP, int lim, float alfaB, int mB, float alfaI){
+    float** tranMatInv = reverseMat(tranMat)
+    float* s = selectSeed()
+}
+
+int main(int argc, char** argv){
+    
+}
